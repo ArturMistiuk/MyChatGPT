0265626dc688665261a5fb93b1f056072b3b56f5
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-02-07 21:30:32 +0100

    Update MyChatGPT.py

--- MyChatGPT.py
+++ MyChatGPT.py
@@ -1,6 +1,13 @@
+"""
+This is a console helper bot that recognizes commands entered
+from the keyboard and responds to this command.
+"""
+
+
 import re
 
 
+# A decorator block to handle user input errors.
 def input_error(func):
     def inner(arguments):
         try:
@@ -13,14 +20,15 @@ def input_error(func):
     return inner
 
 
+# In this block, the bot saves a new contact in memory.
 def add_contact(name, phone):
-    if name in contacts:
+    if name in contacts:    # Checking for an already existing name in memory
         return f'Contact with {name} already created. Try to change it.'
-    match = re.match(r'\+\d{12}', phone)
-    if match:
-        contacts[name] = phone
+    match = re.match(r'\+\d{12}', phone)    # Pattern for phone number
+    if match:    # If number matches the pattern
+        contacts[name] = phone    # Add new contact with name and phone number
         return f'New contact {name} with number {phone} has been added'
-    else:
+    else:   # If phone number doesn't match pattern
         return 'Incorrect number! Write in the format: +380123456789'
 
 
@@ -29,11 +37,12 @@ def advice():
     return instruction
 
 
+# This function changes phone number in a existing contact
 def change_number(name, phone):
-    if name in contacts:
-        contacts[name] = phone
+    if name in contacts:    # Checks that contact with given name is exist
+        contacts[name] = phone    # Number changing
         return f'Contact {name} has been changed'
-    else:
+    else:    # If contact with name doesn't exist
         return f'{name} does not exist in contacts. Try to create new contact.'
 
 
@@ -42,6 +51,7 @@ def close_bot():
     return instruction
 
 
+# Currying
 @input_error
 def handler(command):
     if command in COMMANDS:
@@ -50,24 +60,28 @@ def handler(command):
         return COMMANDS_WITHOUT_ARGS[command]
 
 
+# Shows all contacts
 def get_contacts():
     return contacts
 
 
+# Shows all phone numbers
 def get_phone(name):
     return f"{name}'s phone number is {contacts[name]}"
 
 
+# Handling user commands
 @input_error
 def reply(user_command):
-    if user_command.lower() not in COMMANDS_WITHOUT_ARGS:
-        command, args = user_command.split(' ')[0].lower(), user_command.split(' ')[1:]
-        instruction = handler(command)
-        return instruction(*args)
+    if user_command.lower() not in COMMANDS_WITHOUT_ARGS:    # Checking if given command has arguments
+        command, args = user_command.split(' ')[0].lower(), user_command.split(' ')[1:]    # Separate command, arguments
+        instruction = handler(command)    # Instruction is a signature of given function by user
+        return instruction(*args)    # Execute command with arguments given by user
     else:
-        return handler(user_command.lower())()
+        return handler(user_command.lower())()    # Execute command without any arguments
 
 
+# List of commands that don't take arguments and their command-words
 COMMANDS_WITHOUT_ARGS = {
     'close': close_bot,
     'exit': close_bot,
@@ -75,11 +89,13 @@ COMMANDS_WITHOUT_ARGS = {
     'hello': advice,
     'show all': get_contacts,
 }
+# # List of commands that take arguments and their command-words
 COMMANDS = {
     'add': add_contact,
     'change': change_number,
     'phone': get_phone,
 }
+# Book of contacts
 contacts = {}
 
 
@@ -87,8 +103,8 @@ def main():
     bot_loop = True
     while bot_loop:
         user_input = input('>> ')
-        if handler(user_input.lower()) == close_bot:
-            bot_loop = False
+        if handler(user_input.lower()) == close_bot:    # If the command entered is to exit the program
+            bot_loop = False    # Stop loop
         print('<<', reply(user_input))
 
 
